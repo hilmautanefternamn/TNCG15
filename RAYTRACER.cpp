fe92e56bbff6bf3de3e3f57769b0d600efc75899
Hilm <hilmautanefternamn@gmail.com>
2019-09-17 17:02:15 +0200

    ray intersection pill

--- RAYTRACER.cpp
+++ RAYTRACER.cpp
@@ -1,4 +1,3 @@
-#include "bitmap_image.hpp"
 #include "../SDL2/include/SDL.h"
 #include <iostream>
 #include <math.h>
@@ -15,6 +14,10 @@ public:
 
     Vertex(double inX, double inY, double inZ, double inW)
         : x(inX), y(inY), z(inZ), w(inW) {};
+
+    Vertex operator-(const Vertex &v) const { return Vertex(x - v.x, y - v.y, z - v.z, 1); }
+
+    Vertex crossProduct(const Vertex &v) const { return Vertex(y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x, 1); };
 
     void printVertex()
     {
@@ -48,7 +51,8 @@ red, green and blue channel.*/
 class ColorDbl
 {
 public:
-    ColorDbl() {};  // Default constructor
+    ColorDbl() // Default constructor
+        :red(0.0), green(0.0), blue(0.0) {};
 
     ColorDbl(double R, double G, double B)
         : red(R), green(G), blue(B) {};
@@ -74,11 +78,12 @@ class Ray
 public:
     Ray() {};
 
-    Ray(Vertex s, Vertex e, ColorDbl c) 
+    Ray(const Vertex &s, const Vertex &e, ColorDbl &c) 
     : start(s), end(e), color(c){};
 
-private:
     Vertex start{}, end{};
+
+private:
    // Triangle &triEnd; 
     ColorDbl color;
 };
@@ -87,20 +92,32 @@ private:
 The Triangle has a color, which we represent by an instance of ColorDbl.
 The triangle’s normal direction is stored in an instance of Direction.
 It has a method rayIntersection(Ray arg) that computes the intersection
-between a Ray and the Triangle with the M¨oller-Trumbore algorithm.
+between a Ray and the Triangle with the Möller-Trumbore algorithm.
 */
 class Triangle
 {
 public:
     Triangle() {};
 
-    Triangle(Vertex inV1, Vertex inV2, Vertex inV3, Direction n, ColorDbl c)
-        :v1(inV1), v2(inV2), v3(inV3), normal(n), color(c) {};
+    Triangle(Vertex inv0, Vertex inv1, Vertex inv2, Direction n, ColorDbl c)
+        :v0(inv0), v1(inv1), v2(inv2), normal(n), color(c) {};
 
-    void rayIntersection(Ray arg) {};
+    //computes the intersection between a Ray and the Triangle with the Möller - Trumbore algorithm
+    bool rayIntersection(const Ray &ray) const
+    {
+        // t ?
+        Vertex T = ray.start - v0;
+        Vertex E1 = v1 - v0;
+        Vertex E2 = v2 - v0;
+        Vertex D = ray.end - ray.start;
+        Vertex P = D.crossProduct(E2);
+        Vertex Q = T.crossProduct(E1);
+
+        return false;
+    };
 
 private:
-    Vertex v1, v2, v3;
+    Vertex v0, v1, v2;
     Direction normal;
     ColorDbl color;
 
@@ -130,8 +147,8 @@ The scene image is stored in the ColorDbl attribute of the Pixels.*/
 class Pixel
 {
 public:
-    Pixel() 
-    : color(ColorDbl(1.0, 1.0, 1.0)) {};
+    Pixel() {};
+    //: color(ColorDbl(1.0, 1.0, 1.0)) {};
 
     Pixel(ColorDbl c) 
     : color(c) {};
@@ -157,127 +174,113 @@ triangle and assign the triangle color to the ray. */
 class Camera
 {
 public:
-    Camera(int eye) 
-    : eye(eye){};
+    Camera() 
+    : eye(1){};
 
-    // convert all Pixel::color (vec3) into a 2D array (:
-    void createImage()
-    {
-
-        cout << plane[0][0].getColor();
-       /*
-        for (int i = 0; i < 800; i++)
-            for (int j = 0; j < 800; j++)
-                cout << plane[i][j].getColor();*/
-    }   
+    void setEye()
+    {}
 
     void render()
     {
-        Ray hej{ eye1, eye2, ColorDbl() };
+        //Ray hej{ eye1, eye2, ColorDbl() };
+
+        for (int x = 0; x < 800; x++)
+        {
+            for (int y = 0; y < 800; y++)
+            {
+
+            }
+        }
     }
+   
 
 private:
     int eye; // 1 or 2
     Vertex eye1{ -2.0, 0.0, 0.0, 1.0 };
     Vertex eye2{ -1.0, 0.0, 0.0, 1.0 };
-    Pixel plane[800][800];
+   // Pixel *imagePlane[400][400];
+    
 };
 
 
 // MAIN BITCH   
 int main(int argc, char* argv[])
 {
-    double w = 1.0;
-    std::cout << "Hello World!\n";
+    // Triangle soup
+    {
+        double w = 1.0;
+        std::cout << "Hello World!\n";
 
-    Vertex vertices[14];
-    vertices[0] = Vertex(-3.0, 0.0, -5.0, w);
-    vertices[1] = Vertex(5.0, 0.0, -5.0, w);
-    vertices[2] = Vertex(0.0, 6.0, -5.0, w);
-    vertices[3] = Vertex(10.0, 6.0, -5.0, w);
-    vertices[4] = Vertex(13.0, 0.0, -5.0, w);
-    vertices[5] = Vertex(10.0, -6.0, -5.0, w);
-    vertices[6] = Vertex(0.0, -6.0, -5.0, w);
+        Vertex vertices[14];
+        vertices[0] = Vertex(-3.0, 0.0, -5.0, w);
+        vertices[1] = Vertex(5.0, 0.0, -5.0, w);
+        vertices[2] = Vertex(0.0, 6.0, -5.0, w);
+        vertices[3] = Vertex(10.0, 6.0, -5.0, w);
+        vertices[4] = Vertex(13.0, 0.0, -5.0, w);
+        vertices[5] = Vertex(10.0, -6.0, -5.0, w);
+        vertices[6] = Vertex(0.0, -6.0, -5.0, w);
 
-    vertices[7] = Vertex(-3.0, 0.0, 5.0, w);
-    vertices[8] = Vertex(5.0, 0.0, 5.0, w);
-    vertices[9] = Vertex(0.0, 6.0, 5.0, w);
-    vertices[10] = Vertex(10.0, 6.0, 5.0, w);
-    vertices[11] = Vertex(13.0, 0.0, 5.0, w);
-    vertices[12] = Vertex(10.0, -6.0, 5.0, w);
-    vertices[13] = Vertex(0.0, -6.0, 5.0, w);
+        vertices[7] = Vertex(-3.0, 0.0, 5.0, w);
+        vertices[8] = Vertex(5.0, 0.0, 5.0, w);
+        vertices[9] = Vertex(0.0, 6.0, 5.0, w);
+        vertices[10] = Vertex(10.0, 6.0, 5.0, w);
+        vertices[11] = Vertex(13.0, 0.0, 5.0, w);
+        vertices[12] = Vertex(10.0, -6.0, 5.0, w);
+        vertices[13] = Vertex(0.0, -6.0, 5.0, w);
 
-    /*for(int i = 0; i < 14; ++i )
-        vertices[i].printVertex();*/
+        /*for(int i = 0; i < 14; ++i )
+            vertices[i].printVertex();*/
 
-    Direction normals[8];
-    normals[0] = Direction(0.0, 0.0, 1.0);                  // floor
-    normals[1] = Direction(0.0, 0.0, -1.0);                 // roof
-    normals[2] = Direction(2 / sqrt(5), -1 / sqrt(5), 0.0);     // VB
-    normals[3] = Direction(0.0, -1.0, 0.0);                 // B
-    normals[4] = Direction(-2 / sqrt(5), -1 / sqrt(5), 0.0);    // HB
-    normals[5] = Direction(-2 / sqrt(5), 1 / sqrt(5), 0.0);     // HF
-    normals[6] = Direction(0.0, 1.0, 0.0);                  // F
-    normals[7] = Direction(2 / sqrt(5), 1 / sqrt(5), 0.0);      // VF
+        Direction normals[8];
+        normals[0] = Direction(0.0, 0.0, 1.0);                  // floor
+        normals[1] = Direction(0.0, 0.0, -1.0);                 // roof
+        normals[2] = Direction(2 / sqrt(5), -1 / sqrt(5), 0.0);     // VB
+        normals[3] = Direction(0.0, -1.0, 0.0);                 // B
+        normals[4] = Direction(-2 / sqrt(5), -1 / sqrt(5), 0.0);    // HB
+        normals[5] = Direction(-2 / sqrt(5), 1 / sqrt(5), 0.0);     // HF
+        normals[6] = Direction(0.0, 1.0, 0.0);                  // F
+        normals[7] = Direction(2 / sqrt(5), 1 / sqrt(5), 0.0);      // VF
 
-    /*for (int i = 0; i < 8; ++i)
-        normals[i].printDirection(); */
-    ColorDbl temp;
+        /*for (int i = 0; i < 8; ++i)
+            normals[i].printDirection(); */
+        ColorDbl temp;
 
-    Triangle triangles[24];
-    // floor
-    triangles[0] = Triangle(vertices[1], vertices[2], vertices[0], normals[0], temp);
-    triangles[1] = Triangle(vertices[1], vertices[3], vertices[2], normals[0], temp);
-    triangles[2] = Triangle(vertices[1], vertices[4], vertices[3], normals[0], temp);
-    triangles[3] = Triangle(vertices[1], vertices[5], vertices[4], normals[0], temp);
-    triangles[4] = Triangle(vertices[1], vertices[6], vertices[5], normals[0], temp);
-    triangles[5] = Triangle(vertices[1], vertices[0], vertices[6], normals[0], temp);
-    // roof
-    triangles[6] = Triangle(vertices[8], vertices[9], vertices[7], normals[1], temp);
-    triangles[7] = Triangle(vertices[8], vertices[10], vertices[9], normals[1], temp);
-    triangles[8] = Triangle(vertices[8], vertices[11], vertices[10], normals[1], temp);
-    triangles[9] = Triangle(vertices[8], vertices[12], vertices[11], normals[1], temp);
-    triangles[10] = Triangle(vertices[8], vertices[13], vertices[12], normals[1], temp);
-    triangles[11] = Triangle(vertices[8], vertices[7], vertices[13], normals[1], temp);
-    // VB
-    triangles[12] = Triangle(vertices[2], vertices[7], vertices[0], normals[2], temp);
-    triangles[13] = Triangle(vertices[2], vertices[9], vertices[7], normals[2], temp);
-    // B
-    triangles[14] = Triangle(vertices[3], vertices[9], vertices[2], normals[3], temp);
-    triangles[15] = Triangle(vertices[3], vertices[10], vertices[9], normals[3], temp);
-    // HB
-    triangles[16] = Triangle(vertices[4], vertices[10], vertices[3], normals[4], temp);
-    triangles[17] = Triangle(vertices[4], vertices[11], vertices[10], normals[4], temp);
-    // HF
-    triangles[18] = Triangle(vertices[5], vertices[11], vertices[4], normals[5], temp);
-    triangles[19] = Triangle(vertices[5], vertices[12], vertices[11], normals[5], temp);
-    // F
-    triangles[20] = Triangle(vertices[6], vertices[12], vertices[5], normals[6], temp);
-    triangles[21] = Triangle(vertices[6], vertices[13], vertices[12], normals[6], temp);
-    // VF
-    triangles[22] = Triangle(vertices[0], vertices[13], vertices[6], normals[7], temp);
-    triangles[23] = Triangle(vertices[0], vertices[7], vertices[13], normals[7], temp);
+        Triangle triangles[24];
+        // floor
+        triangles[0] = Triangle(vertices[1], vertices[2], vertices[0], normals[0], temp);
+        triangles[1] = Triangle(vertices[1], vertices[3], vertices[2], normals[0], temp);
+        triangles[2] = Triangle(vertices[1], vertices[4], vertices[3], normals[0], temp);
+        triangles[3] = Triangle(vertices[1], vertices[5], vertices[4], normals[0], temp);
+        triangles[4] = Triangle(vertices[1], vertices[6], vertices[5], normals[0], temp);
+        triangles[5] = Triangle(vertices[1], vertices[0], vertices[6], normals[0], temp);
+        // roof
+        triangles[6] = Triangle(vertices[8], vertices[9], vertices[7], normals[1], temp);
+        triangles[7] = Triangle(vertices[8], vertices[10], vertices[9], normals[1], temp);
+        triangles[8] = Triangle(vertices[8], vertices[11], vertices[10], normals[1], temp);
+        triangles[9] = Triangle(vertices[8], vertices[12], vertices[11], normals[1], temp);
+        triangles[10] = Triangle(vertices[8], vertices[13], vertices[12], normals[1], temp);
+        triangles[11] = Triangle(vertices[8], vertices[7], vertices[13], normals[1], temp);
+        // VB
+        triangles[12] = Triangle(vertices[2], vertices[7], vertices[0], normals[2], temp);
+        triangles[13] = Triangle(vertices[2], vertices[9], vertices[7], normals[2], temp);
+        // B
+        triangles[14] = Triangle(vertices[3], vertices[9], vertices[2], normals[3], temp);
+        triangles[15] = Triangle(vertices[3], vertices[10], vertices[9], normals[3], temp);
+        // HB
+        triangles[16] = Triangle(vertices[4], vertices[10], vertices[3], normals[4], temp);
+        triangles[17] = Triangle(vertices[4], vertices[11], vertices[10], normals[4], temp);
+        // HF
+        triangles[18] = Triangle(vertices[5], vertices[11], vertices[4], normals[5], temp);
+        triangles[19] = Triangle(vertices[5], vertices[12], vertices[11], normals[5], temp);
+        // F
+        triangles[20] = Triangle(vertices[6], vertices[12], vertices[5], normals[6], temp);
+        triangles[21] = Triangle(vertices[6], vertices[13], vertices[12], normals[6], temp);
+        // VF
+        triangles[22] = Triangle(vertices[0], vertices[13], vertices[6], normals[7], temp);
+        triangles[23] = Triangle(vertices[0], vertices[7], vertices[13], normals[7], temp);
+    }
 
-/*
-    bitmap_image image(200, 200);
-
-    // set background to orange
-    image.set_all_channels(255, 150, 50);
-
-    image_drawer draw(image);
-
-    draw.pen_width(3);
-    draw.pen_color(255, 0, 0);
-    draw.circle(image.width() / 2, image.height() / 2, 50);
-
-    draw.pen_width(1);
-    draw.pen_color(0, 0, 255);
-    draw.rectangle(50, 50, 150, 150);
-
-    image.save_image("output.bmp");
-*/
-    
-    //Camera c{1};
+    Camera c{};
 
     //c.createImage();
 

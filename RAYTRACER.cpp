86a90321354117466f175e56ea1186b429efd0ef
Hilm <hilmautanefternamn@gmail.com>
2019-09-20 10:28:54 +0200

    lite kladd

--- RAYTRACER.cpp
+++ RAYTRACER.cpp
@@ -93,7 +93,7 @@ private:
 The Triangle has a color, which we represent by an instance of ColorDbl.
 The triangle�s normal direction is stored in an instance of Direction.
 It has a method rayIntersection(Ray arg) that computes the intersection
-between a Ray and the Triangle with the M�ller-Trumbore algorithm.
+between a Ray and the Triangle with the Möller-Trumbore algorithm.
 */
 class Triangle
 {
@@ -103,7 +103,7 @@ public:
     Triangle(Vertex inv0, Vertex inv1, Vertex inv2, Direction n, ColorDbl c)
         :v0(inv0), v1(inv1), v2(inv2), normal(n), color(c) {};
 
-    //computes the intersection between a Ray and the Triangle with the M�ller - Trumbore algorithm
+    //computes the intersection between a Ray and the Triangle with the Möller - Trumbore algorithm
     bool rayIntersection(const Ray &ray) const
     {
         // t ?
@@ -178,8 +178,7 @@ public:
     Camera() 
     : eye(1){};
 
-    void setEye()
-    {}
+    void setEye(int e) { eye = e; }
 
     void render()
     {

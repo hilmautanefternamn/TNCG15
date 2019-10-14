1601c67f5da66320e0b99de07d10c488a39b9666
Unknown <danielsahlin8@gmail.com>
2019-10-14 10:50:21 +0200

    shadowrays

--- Tetrahedron.cpp
+++ Tetrahedron.cpp
@@ -27,15 +27,18 @@ public:
     // arg Phit = intersection point (vertex)
     bool rayIntersection(Ray &ray, double &t, Vertex &Phit, Direction &normal)
     {
+		Vertex tetraHit;
 		double tMin = 10000.0;
 		bool isHit = false;
         for (auto &tri : trianglesTetra)
         {
-			if (tri.rayIntersection(ray, t, Phit) && t < tMin) 
+			if (tri.rayIntersection(ray, t, tetraHit) && t < tMin) 
 			{
 				tMin = t;
 				isHit = true;
 				normal = tri.getNormal();
+				Phit = tetraHit;
+
 			}
         }
 
